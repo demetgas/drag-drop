9c07a438f0df72d1d1be0da1a52ca174301ab4c6
Demet Gaş <77278951+demetgas@users.noreply.github.com>
2024-03-22 10:44:04 +0100

    css

--- drop-down/src/components/Task/Task.jsx
+++ drop-down/src/components/Task/Task.jsx
@@ -4,7 +4,7 @@ import data from "../../data/data";
 
 export default function Task(props) {
   return (
-    <div>
+    <div className="cardContainer">
       {data.map((item, index) => {
         const category = Object.keys(item)[0]; // Get the category (e.g., todo, pending, done)
         const tasks = item[category]; // Get the tasks for the category
