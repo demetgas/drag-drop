16b9d301a7d9d049741aeb0bc04022d8a01bc0ff
Demet Gaş <77278951+demetgas@users.noreply.github.com>
2024-03-22 09:29:35 +0100

    Update Card.jsx

---
+++ drop-down/src/components/Card/Card.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+export default function Card(props) {
+  return (
+    <div>
+      <div className="card">
+        <div className="header">
+          <div className="title">hello</div>
+        </div>
+        <div className="list">
+          <div className="listItem">hello</div>
+        </div>
+      </div>
+    </div>
+  );
+}
