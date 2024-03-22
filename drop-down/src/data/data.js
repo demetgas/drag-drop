3b0fc1cbb785bb072c2244f22f051dc127247e7d
Demet Gaş <77278951+demetgas@users.noreply.github.com>
2024-03-22 10:19:32 +0100

    created data

---
+++ drop-down/src/data/data.js
@@ -0,0 +1,22 @@
+const data = [
+  {
+    toDo: {
+      task1: "cook",
+      task2: "finish teh dishes",
+      task3: "start the washing machine",
+      task4: "groceries",
+    },
+  },
+  {
+    pending: {
+      task5: "get the cargo",
+    },
+  },
+  {
+    done: {
+      task6: "buy the books",
+    },
+  },
+];
+
+export default data;
