710eb9700cdceaf6c09951c61772b2a6229bed62
Demet Gaş <77278951+demetgas@users.noreply.github.com>
2024-03-25 10:37:01 +0100

    error fixed

--- drop-down/src/components/Task/Task.jsx
+++ drop-down/src/components/Task/Task.jsx
@@ -62,6 +62,7 @@ export default function Task(props, id) {
                         )}
                       </Draggable>
                     ))}
+                    {provided.placeholder}
                   </div>
                 )}
               </Droppable>
