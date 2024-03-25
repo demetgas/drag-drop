25966e67cf386746cf7776b9a203b2332f2dba11
Demet Gaş <77278951+demetgas@users.noreply.github.com>
2024-03-25 10:04:19 +0100

    Update Card.jsx

--- drop-down/src/components/Card/Card.jsx
+++ drop-down/src/components/Card/Card.jsx
@@ -1,14 +1,11 @@
 import React from "react";
 import Task from "../Task/Task";
 import "./card.css";
-import { DragDropContext } from "react-beautiful-dnd";
 export default function Card(props) {
   return (
     <div className="container">
       <div className="card">
-        <DragDropContext>
-          <Task />
-        </DragDropContext>
+        <Task />
       </div>
     </div>
   );
