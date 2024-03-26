b08a9e0aa894057fc5b50bbd27a25e0e56128eef
Demet Gaş <77278951+demetgas@users.noreply.github.com>
2024-03-26 16:26:50 +0100

    Update data.js

--- drop-down/src/data/data.js
+++ drop-down/src/data/data.js
@@ -2,37 +2,37 @@ const data = [
   {
     id: "To Do",
     tasks: {
-      task1: "Task 1",
-      task2: "Task 2",
-      task3: "Task 3",
-      task4: "Task 4",
-      task5: "Task 5",
-      task6: "Task 6",
-      task7: "Task 7",
-      task8: "Task 8 ",
+      task1: "task1",
+      task2: "task2",
+      task3: "task3",
+      task4: "task4",
+      task5: "task5",
+      task6: "task6",
+      task7: "task7",
+      task8: "task8 ",
     },
   },
   {
     id: "In Progress",
     tasks: {
-      task9: "Task 9",
-      task10: "Task 10 ",
+      task9: "task9",
+      task10: "task10 ",
     },
   },
   {
     id: "Done",
     tasks: {
-      task11: "Task 11",
-      task12: "Task 12 ",
-      task13: "Task 13",
+      task11: "task11",
+      task12: "task12 ",
+      task13: "task13",
     },
   },
   {
     id: "Even",
     tasks: {
-      task14: "Task 14",
-      task16: "Task 16 ",
-      task18: "Task 18",
+      task14: "task14",
+      task16: "task16 ",
+      task18: "task18",
     },
   },
 ];
