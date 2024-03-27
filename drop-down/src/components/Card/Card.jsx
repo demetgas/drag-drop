c89e2227f9b3b3c02590e4ee480b1ef7580f3e9d
Demet Gaş <77278951+demetgas@users.noreply.github.com>
2024-03-27 16:18:15 +0100

    created handleDragEnd, getStyles

--- drop-down/src/components/Card/Card.jsx
+++ drop-down/src/components/Card/Card.jsx
@@ -1,15 +1,44 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import "./card.css";
 import data from "../../data/data";
 
 export default function Card() {
   const [array, setArray] = useState(data);
   const [showMoreTasks, setShowMoreTasks] = useState({});
+  const [dragging, setDragging] = useState(false);
 
-  const handleDragStart = (e, taskName) => {
+  const dragItem = useRef();
+  const dragItemNode = useRef();
+
+  const handleDragStart = (e, taskName, params) => {
+    console.log("gello", params);
     e.dataTransfer.setData("id", taskName);
+    dragItem.current = params;
+    dragItemNode.current = e.target;
+    dragItemNode.current.addEventListener("dragend", handleDragEnd);
+    setDragging(true);
+  };
+  const getStyles = (params) => {
+    const currentItem = dragItem.current;
+    if (
+      currentItem &&
+      currentItem.taskId === params.id &&
+      currentItem.task.name === params.task.name
+    ) {
+      return { backgroundColor: "yellow" }; // Set the desired background color here
+    }
+    return null;
   };
 
+  const handleDragEnd = () => {
+    console.log("bye");
+    setDragging(false);
+    if (dragItemNode.current) {
+      dragItemNode.current.removeEventListener("dragend", handleDragEnd);
+    }
+    dragItem.current = null;
+    dragItemNode.current = null;
+  };
   const handleDrop = (e, newTitleId) => {
     const taskId = e.dataTransfer.getData("id");
     const taskNumber = parseInt(taskId.replace("task", ""));
@@ -20,7 +49,7 @@ export default function Card() {
     const updatedData = array.map((titleName) => {
       if (titleName.id === newTitleId) {
         //if the task gets dropped to the same card return
-        if (titleName.tasks.find((task) => task.name === taskId)) {
+        if (titleName.tasks.find((task) => task.name === taskNumber)) {
           return titleName;
         }
         //add the task to the new card
@@ -72,7 +101,10 @@ export default function Card() {
                   key={taskIndex}
                   className="listItem"
                   draggable
-                  onDragStart={(e) => handleDragStart(e, task.name)}
+                  onDragStart={(e) =>
+                    handleDragStart(e, task.name, { id, tasks })
+                  }
+                  style={dragging ? getStyles({ id, tasks }) : null}
                 >
                   {task.name}
                 </div>
